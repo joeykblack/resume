2b4dcecbb72849e292c22e79550a5314135d18ec
joeykblack <joseph.black1@navy.mil>
2015-03-16 14:47:39 -0400

    Short/Long NDMS rolls resumes initial

--- gen_resume_md.py
+++ gen_resume_md.py
@@ -8,18 +8,28 @@ header_with_phone = """joeykblack.github.io
 header_with_links = """[Home page](http://joeykblack.github.io)
 [PDF Resume](http://joeykblack.github.io/resume/joeykblack_resume.pdf)"""
 
-def output_template(header, filename):
+def output_template(header, ndms_rolls, filename):
     with open ("resume.template", "r") as template_file:
         template = template_file.read()
-        d = dict(header=header)
+        d = dict(header=header, ndms_rolls=ndms_rolls)
         result = Template(template).substitute(d)
         with open(filename, "w") as output_file:
             output_file.write(result)
 
+def load(fileName):
+    with open (fileName, "r") as part:
+        part_string = part.read()
+		return part_string
+
 def main():
-    output_template(header, "joeykblack_resume.md")
-    output_template(header_with_phone, "joeykblack_resume_with_phone.md")
-    output_template(header_with_links, "index.md")
+    ndms_rolls_short = load("ndms_rolls_short.part")
+	
+    output_template(header, ndms_rolls_short, "joeykblack_resume.md")
+    output_template(header_with_phone, ndms_rolls_short, "joeykblack_resume_with_phone.md")
+    output_template(header_with_links, ndms_rolls_short, "index.md")
+	
+    ndms_rolls_long = load("ndms_rolls_long.part")
+    output_template(header_with_phone, ndms_rolls_long, "joeykblack_resume_with_phone_for_dod.md"
 
 
 
